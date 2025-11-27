acf6cb68299134a32c553a0a40af1f210f2fbcd5
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2025-11-27 19:48:41 +0200

    feat(user): integrate form and server action for signup

--- apps/server/src/users/users.controller.ts
+++ apps/server/src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserRequest } from './dto/create-user.request';
+import { NoFilesInterceptor } from '@nestjs/platform-express';
 
 
 @Controller('users')
@@ -8,6 +9,7 @@ export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Post()
+    @UseInterceptors(NoFilesInterceptor())
     createUser(@Body() request: CreateUserRequest) {
         return this.usersService.createUser(request);
     }
