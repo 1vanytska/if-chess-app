2430f4758dd167616e270eeac2bcfb0d6d6ff6d8
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2025-11-28 19:54:35 +0200

    feat(auth): add cookie-based authentication

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -1,14 +1,46 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import ms from 'ms';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from '../users/users.service';
+import { Response } from 'express';
+import { User } from 'prisma/generated';
+import { ConfigService } from '@nestjs/config';
+import { TokenPayload } from './token-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService,
+        private readonly configService: ConfigService,
+        private readonly jwtService: JwtService,
+    ) { }
+
+    async login(user: User, response: Response) {
+        const expires = new Date();
+        expires.setMilliseconds(
+            expires.getMilliseconds() +
+            ms(
+                this.configService.getOrThrow<string>(
+                    'JWT_EXPIRATION') as unknown as ms.StringValue,
+            ),
+        );
+
+        const tokenPayload: TokenPayload = {
+            userId: user.id,
+        };
+        const token = this.jwtService.sign(tokenPayload);
+
+        response.cookie('Authentication', token, {
+            secure: true,
+            httpOnly: true
+        });
+
+        return { tokenPayload };
+    }
 
     async verifyUser(email: string, password: string) {
         try {
-            const user =await this.usersService.getUser({ email });
+            const user = await this.usersService.getUser({ email });
             const authenticated = await bcrypt.compare(password, user.password);
             if (!authenticated) {
                 throw new UnauthorizedException();
